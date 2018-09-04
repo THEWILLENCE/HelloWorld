91682eac4709ecc92eafbbfb3caee0593f0cddfc
william.ence <william.ence@CLC-LAB209-16.TCSD.tooelesd.org>
2018-09-04 13:25:38 -0600

    first commit

---
+++ src/Will/Ence1helloWorld/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * hello world program prints hello world in console
+ */
+package Will.Ence1helloWorld;
+
+/**
+ * @author william.ence
+ * 9/4/18
+ * hello world
+ * 
+ */ 
+public class HelloWorld {
+
+	  
+	 
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+
+	}
+
+}
